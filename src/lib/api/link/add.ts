3f384f6f8be3e997b1e239d2bd7f9ab3973d8eb0
HuuThongg <103173514+HuuThongg@users.noreply.github.com>
2024-10-09 17:45:11 -0700

    add tag

--- src/lib/api/link/add.ts
+++ src/lib/api/link/add.ts
@@ -10,46 +10,46 @@ export let errorInvalidUrl = '';
 const myLinks: Link[] = [];
 
 export async function addLink(url: string, folderID: string): Promise<Link | undefined> {
-	const s = getSession();
-	if (!s) return;
+  const s = getSession();
+  if (!s) return;
 
-	const response = await fetch(`${PUBLIC_API_ENDPOINT}/private/tag/add`, {
-		method: 'POST', // *GET, POST, PUT, DELETE, etc.
-		mode: 'cors', // no-cors, *cors, same-origin
-		cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-		credentials: 'include', // include, *same-origin, omit
-		headers: {
-			'Content-Type': 'application/json',
-			authorization: `Bearer ${s.access_token}`
-			// 'Content-Type': 'application/x-www-form-urlencoded',
-		},
-		redirect: 'follow', // manual, *follow, error
-		referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-		body: JSON.stringify({
-			url: url,
-			folder_id: folderID
-		}) // body data type must match "Content-Type" header
-	});
+  const response = await fetch(`${PUBLIC_API_ENDPOINT}/private/link/add`, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'include', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json',
+      authorization: `Bearer ${s.access_token}`
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify({
+      url: url,
+      folder_id: folderID
+    }) // body data type must match "Content-Type" header
+  });
 
-	const result = await response.json();
+  const result = await response.json();
 
-	if (result[0] === null) {
-		console.log('no result returned');
+  if (result[0] === null) {
+    console.log('no result returned');
 
-		return;
-	}
+    return;
+  }
 
-	if (result.message) {
-		return;
-	}
+  if (result.message) {
+    return;
+  }
 
-	const link: Link = result[0];
+  const link: Link = result[0];
 
-	if (get(links) !== null) {
-		console.log('set link');
-		links.update((values) => [link, ...values]);
-	} else {
-		links.set([...myLinks, link]);
-	}
-	return link;
+  if (get(links) !== null) {
+    console.log('set link');
+    links.update((values) => [link, ...values]);
+  } else {
+    links.set([...myLinks, link]);
+  }
+  return link;
 }
