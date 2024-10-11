fb204799836e5c8cdb8061598478e210054eecc3
HuuThongg <103173514+HuuThongg@users.noreply.github.com>
2024-10-10 21:37:10 -0700

    ds

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,13 +1,18 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
-	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface PageState {}
-		// interface Platform {}
-	}
+  namespace App {
+    // interface Error {}
+    // interface Locals {}
+    // interface PageData {}
+    // interface PageState {}
+    interface Platform {
+      env?: {
+        YOUR_KV_NAMESPACE: KVNamespace;
+        YOUR_DURABLE_OBJECT_NAMESPACE: DurableObjectNamespace;
+      };
+    }
+  }
 }
 
-export {};
+export { };
